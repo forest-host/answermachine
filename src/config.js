0f153954901e45d16f84fc39e7786da3689e91e5
Johan Schuijt <johan@publicpixels.nl>
2020-03-27 19:40:30 +0100

    fix config env var

--- src/config.js
+++ src/config.js
@@ -5,7 +5,7 @@ if(process.env.NODE_ENV != 'testing') {
   config.knex.connection.host = process.env.DATABASE_MAIN_HOST;
   config.knex.connection.user = process.env.DATABASE_MAIN_USER;
   config.knex.connection.password = process.env.DATABASE_MAIN_PASSWORD;
-  config.knex.connection.database = process.env.DATABASE_MAIN_DATABASE;
+  config.knex.connection.database = process.env.DATABASE_MAIN_NAME;
 
   config.elastic.node = `http://${process.env.ELASTICSEARCH_HOST}:${process.env.ELASTICSEARCH_PORT}`
 }
